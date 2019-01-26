9c4ee30bc87880c6a04f0b7dbbca55c3a0e6afba
Lawrence Obrero <lawrence.obrero79@gmail.com>
2019-01-26 14:02:11 -0800

    initial commit

---
+++ src/main/java/org/lobrero/spring5webapppractice/Spring5webappPracticeApplication.java
@@ -0,0 +1,14 @@
+package org.lobrero.spring5webapppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5webappPracticeApplication.class, args);
+    }
+
+}
+
